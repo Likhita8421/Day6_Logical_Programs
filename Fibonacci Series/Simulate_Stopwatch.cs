fb7f321f2121133c57bfa82284c85b09799c4e6d
HP <leenupandagre24@gmail.com>
2022-05-23 23:50:12 +0530

    Stopwatch added

---
+++ Fibonacci Series/Simulate_Stopwatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    internal class Simulate_Stopwatch
+    {
+		Simulate_Stopwatch stopwatchnew = new Stopwatch();
+        public void calculatetime()
+        {
+            stopwatchnew.Start();
+            Console.WriteLine("wait till to press any key 'y' key to stop timer");
+            if (Console.ReadKey().Key == ConsoleKey.S)
+            {
+                stopwatchnew.Stop();
+            }
+            TimeSpan time = stopwatchnew.Elapsed;
+            Console.WriteLine("\nElapsed Time is {0:00}M:{1:00}S",  time.Minutes, time.Seconds);
+        
+        }
+	}
+}
+
