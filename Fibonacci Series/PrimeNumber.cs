0f34aa4af4e33b2a63a92cc04de6213ad4c73a08
HP <leenupandagre24@gmail.com>
2022-05-23 22:55:21 +0530

    prime number added

---
+++ Fibonacci Series/PrimeNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    internal class PrimeNumber
+    {
+        public void prime()
+        {
+            int num1;
+
+            Console.WriteLine("Enter number for prime:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == 0 || num1 == 1)
+            {
+                Console.WriteLine(num1 + " is not prime number");
+                Console.ReadLine();
+            }
+            else
+            {
+                for (int a = 2; a <= num1 / 2; a++)
+                {
+                    if (num1 % a == 0)
+                    {
+                        Console.WriteLine(num1 + " is not prime number");
+                        return;
+                    }
+
+                }
+                Console.WriteLine(num1 + " is a prime number");
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
