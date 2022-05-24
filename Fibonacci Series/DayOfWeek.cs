691ccb8109e6642f62db67ac18e22621f42b797b
HP <leenupandagre24@gmail.com>
2022-05-25 00:20:32 +0530

    day Of week added

---
+++ Fibonacci Series/DayOfWeek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    internal class DayOfWeek
+    {
+        public void DayMonth()
+        {
+            int x, y, m, d;
+            string[] WeekDays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("Enter a date:");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Month:");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a year:");
+            int year = int.Parse(Console.ReadLine());
+
+            y = year - (14 - month) / 12;
+            x = y + y / 4 - y / 100 + y / 400;
+            m = month + 12 * ((14 - month) / 12) - 2;
+            d = (day + x + (31 * m) / 12) % 7;
+
+            Console.WriteLine("its a " + WeekDays[d]);
+        }
+    }
+}
