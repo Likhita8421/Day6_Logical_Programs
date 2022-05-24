62fc4524a796cc39e75a129a196452007ebf1ebc
HP <leenupandagre24@gmail.com>
2022-05-25 01:26:08 +0530

    square root added

---
+++ Fibonacci Series/SquareRoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    internal class SquareRoot
+    {
+        string x;
+        double rootofx;
+        Console.WriteLine("Enter a Number:");
+            x = Console.ReadLine();
+            rootofx = Math.Sqrt(Convert.ToInt32(x));
+            Console.WriteLine("\nSqrt({0}): {1}", x, rootofx);
+            Console.ReadKey();
+    }
+}
